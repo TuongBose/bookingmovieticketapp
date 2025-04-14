dae0230b1f4115d361ec162180a0556828064031
Le Manh Tuong <lemanhtuong2109@gmail.com>
2025-04-15 00:34:27 +0700

    rename

--- backend/bookingmovieticketapp/src/main/java/com/project/bookingmovieticketapp/BookingmovieticketappApplication.java
+++ backend/bookingmovieticketapp/src/main/java/com/project/bookingmovieticketapp/BookingmovieticketappApplication.java
@@ -1,4 +1,4 @@
-package com.example.bookingmovieticketapp;
+package com.project.bookingmovieticketapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/bookingmovieticketapp/src/test/java/com/project/bookingmovieticketapp/BookingmovieticketappApplicationTests.java
+++ backend/bookingmovieticketapp/src/test/java/com/project/bookingmovieticketapp/BookingmovieticketappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bookingmovieticketapp;
+package com.project.bookingmovieticketapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
