25f5edb9d8d167706078ae7c87f53770fbbd7a7c
Le Manh Tuong <lemanhtuong2109@gmail.com>
2025-04-28 22:59:36 +0700

    generate component for sidebar

--- webadmin/src/app/app.module.ts
+++ webadmin/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { CinemaListComponent } from './cinema-list/cinema-list.component';
+import { ShowtimeListComponent } from './showtime-list/showtime-list.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { AdminListComponent } from './admin-list/admin-list.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    MovieListComponent,
+    CinemaListComponent,
+    ShowtimeListComponent,
+    CustomerListComponent,
+    AdminListComponent
   ],
   imports: [
     BrowserModule,
---
+++ webadmin/src/app/cinema-list/cinema-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinema-list',
+  standalone: false,
+  templateUrl: './cinema-list.component.html',
+  styleUrl: './cinema-list.component.css'
+})
+export class CinemaListComponent {
+
+}
